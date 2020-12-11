343f33237e85ed42a0fb1b4c3bf957a420005dfe
Clumsy Knight <clusmsyknight@gmail.com>
2020-12-11 20:24:32 +0530

    included toast component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueToastify from "vue-toastify";
 
+Vue.use(VueToastify, {
+  position: "top-right",
+  theme: "light",
+  draggable: true,
+});
 Vue.config.productionTip = false;
 
 new Vue({
