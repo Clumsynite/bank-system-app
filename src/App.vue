8ebcdace0c4c63229f1f35e446469d1581a3be2b
Clumsy Knight <clusmsyknight@gmail.com>
2020-12-11 17:51:25 +0530

    modified navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <div id="nav" class="w-100 p-4 text-center shadow-sm mb-4">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/signup">Create New Account</router-link>
     </div>
     <router-view />
   </div>
@@ -13,7 +14,6 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
