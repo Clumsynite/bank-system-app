4dc54b35bdf4090527cba8c498dae372d5473935
Clumsy Knight <clusmsyknight@gmail.com>
2020-12-11 20:24:21 +0530

    added required attributes and submit handlers
    called toast

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="login container">
-    <form class="row g-2 w-75 mx-auto shadow p-3 rounded">
+    <form
+      class="row g-2 w-75 mx-auto shadow p-3 rounded"
+      v-on:submit="formSubmit"
+    >
       <h4 class="text-center mb-3 border-bottom border-dark pb-2">
         Login Page
       </h4>
@@ -12,6 +15,8 @@
           placeholder="Ex. rishabh"
           name="username"
           v-model="username"
+          required
+          autofocus
         />
         <label for="username" class="ps-4"> Enter username</label>
       </div>
@@ -23,36 +28,63 @@
           placeholder="Ex. password"
           name="password"
           v-model="password"
+          required
         />
         <label for="password" class="ps-4"> Enter Password</label>
       </div>
       <div class="form-floating mb-3">
-        <select class="form-select" id="acccount" v-model="account">
+        <select class="form-select" id="acccount" v-model="account" required>
           <option value="banker" selected> Banker</option>
           <option value="customer"> Customer</option>
         </select>
         <label for="account">Select Account Type</label>
       </div>
       <button type="submit" class="btn btn-block btn-outline-dark">
-        LOGIN
+        <span v-if="!loading">LOGIN</span>
+        <Spinner size="24" line-fg-color="#212529" v-else />
       </button>
     </form>
   </div>
 </template>
 
 <script>
+import { login } from "../api";
+import Spinner from "vue-simple-spinner";
 export default {
   name: "Login",
+  components: {
+    Spinner,
+  },
   data() {
     return {
       username: "",
       password: "",
       account: "",
+      loading: false,
     };
   },
   methods: {
-    submitForm(value) {
-      console.log(value);
+    async formSubmit(e) {
+      e.preventDefault();
+      try {
+        this.loading = true;
+        const data = await login({
+          username: this.username,
+          password: this.password,
+          account: this.account,
+        });
+        this.loading = false;
+        if (data.msg) {
+          localStorage.setItem("user", JSON.stringify(data.user));
+          localStorage.setItem("token", data.token);
+          this.$vToastify.success(data.msg);
+        } else if (data.err) {
+          this.$vToastify.error(data.err);
+        }
+      } catch (error) {
+        this.loading = false;
+        console.error(error);
+      }
     },
   },
 };
--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="signup container">
-    <form class="row g-2 w-75 mx-auto shadow p-3 rounded">
+    <form
+      class="row g-2 w-75 mx-auto shadow p-3 rounded"
+      v-on:submit="formSubmit"
+    >
       <h4 class="text-center mb-3 border-bottom border-dark pb-2">
         Create New Account
       </h4>
@@ -12,6 +15,8 @@
           placeholder="Ex. Rishabh Pathak"
           name="fullname"
           v-model="fullname"
+          autofocus
+          required
         />
         <label for="fullname" class="ps-4"> Enter your Fullname</label>
       </div>
@@ -23,8 +28,9 @@
           placeholder="Ex. rishabh"
           name="username"
           v-model="username"
+          required
         />
-        <label for="username" class="ps-4"> Enter username</label>
+        <label for="username" class="ps-4"> Enter a unique username</label>
       </div>
       <div class="col-md-6 form-floating mb-3">
         <input
@@ -34,33 +40,64 @@
           placeholder="Ex. password"
           name="password"
           v-model="password"
+          required
         />
         <label for="password" class="ps-4"> Enter Password</label>
       </div>
       <div class="form-floating mb-3">
-        <select class="form-select" id="acccount" v-model="account">
+        <select class="form-select" id="acccount" v-model="account" required>
           <option value="banker" selected> Banker</option>
           <option value="customer"> Customer</option>
         </select>
         <label for="account">Select Account Type</label>
       </div>
       <button type="submit" class="btn btn-block btn-outline-dark">
-        Signup
+        <span v-if="!loading">Signup</span>
+        <Spinner size="24" line-fg-color="#212529" v-else />
       </button>
     </form>
   </div>
 </template>
 
 <script>
+import { signup } from "../api";
+import Spinner from "vue-simple-spinner";
 export default {
   name: "Signup",
+  components: {
+    Spinner,
+  },
   data() {
     return {
       fullname: "",
       username: "",
       password: "",
       account: "",
+      loading: false,
     };
+  },
+  methods: {
+    async formSubmit(e) {
+      e.preventDefault();
+      try {
+        this.loading = true;
+        const data = await signup({
+          name: this.fullname,
+          username: this.username,
+          password: this.password,
+          account: this.account,
+        });
+        this.loading = false;
+        if (data.msg) {
+          this.$vToastify.success(data.msg);
+        } else if (data.err) {
+          this.$vToastify.error(data.err);
+        }
+      } catch (error) {
+        this.loading = false;
+        console.error(error);
+      }
+    },
   },
 };
 </script>
