586afd8570723f744f05be3c1a7b492abd862fd6
Clumsy Knight <clusmsyknight@gmail.com>
2020-12-12 14:54:45 +0530

    added customer profile card on customerProfile

--- src/components/Customer.vue
+++ src/components/Customer.vue
@@ -1,12 +1,33 @@
 <template>
-  <div>
-    <h1>Customer</h1>
+  <div class="profile container">
+    <div class="card text-dark mx-auto bg-light mb-3 w-75 shadow rounded">
+      <div class="card-header text-center">Your Details</div>
+      <div class="card-body">
+        <div class="row">
+          <p class="card-text col">Name: {{ user.name }}</p>
+          <p class="card-text col">Joined: {{ date }}</p>
+        </div>
+        <p class="card-text">username: {{ user.username }}</p>
+        <p class="card-text">Account type: {{ user.account }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { fromNow } from "../helper";
 export default {
   name: "Customer",
+  data() {
+    return {
+      user: JSON.parse(localStorage.user),
+    };
+  },
+  computed: {
+    date: function() {
+      return fromNow(this.user.joined);
+    },
+  },
 };
 </script>
 
